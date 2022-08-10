14f17147ad8340af757215cd91a9950f284ecaa6
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-08-10 09:30:25 -0500

    Two Sum Sum every posibility of the list

---
+++ two_Sum.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for x in range(len(nums)):
+            for y in range(x+1,len(nums)):
+                if nums[x] + nums[y] == target:
+                    return [x, y]
+        
